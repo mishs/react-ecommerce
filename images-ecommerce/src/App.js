0f04caa2331d8910dc5db827e0ce9bf3b62bb64a
Mish <mishuct26@gmail.com>
2021-01-20 00:16:25 +0200

    Cart and Photos links import

--- images-ecommerce/src/App.js
+++ images-ecommerce/src/App.js
@@ -1,6 +1,8 @@
 import React from "react"
 import './App.css';
 import Header from "./components/Header"
+import Cart from "./pages/Cart"
+import Photos from "./pages/Photos"
 
 function App() {    
     return (
