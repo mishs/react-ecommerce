a4b6e7b71a9248397ffeb396992042b36a264f9e
Mish <mishuct26@gmail.com>
2021-01-20 14:35:35 +0200

    Images url update

--- images-ecommerce/src/Context.js
+++ images-ecommerce/src/Context.js
@@ -6,21 +6,15 @@ function ContextProvider({children}){
     const [allPhotos, setAllPhotos] = useState([])
     // ({children}) is equal to props.children. Just that here did destructure
 
-    //Get the data from API
-    const url = "https://github.com/mishs/react-ecommerce/blob/main/images-ecommerce/project-images/images.json"
-    // const noCors = {mode: "no-cors"}
+    const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
     useEffect(() => {
-                //save data to state
-                //fetch API returns promise to pass as json
-            fetch(url)
-            
-                .then(res => res.json(), {mode: "no-cors" })
-                .then(data => setAllPhotos(data))
-                
-    }, [] )
-    // .catch(err => console.error(err));
-
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setAllPhotos(data))
+    }, [])
+    
     console.log(allPhotos)
+    
 
     return (
         <Context.Provider value={{allPhotos}}>
