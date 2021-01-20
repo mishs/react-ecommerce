71aaefecc42e94437880b415adb0739aed10e527
Mish <mishuct26@gmail.com>
2021-01-20 10:41:18 +0200

    value prop for context provider component

--- images-ecommerce/src/Context.js
+++ images-ecommerce/src/Context.js
@@ -5,7 +5,7 @@ const Context = React.createContext()
 function ContextProvider({children}){
     // ({children}) is equal to props.children. Just that here did destructure
     return (
-        <Context.Provider>
+        <Context.Provider value="">
             {children}
             {/* since it's a custom component (to wrap around whole component in our app) */}
         </Context.Provider>
