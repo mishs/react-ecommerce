b18947c8f17bb2425aef3fc8efd2280bd1b6627c
Mish <mishuct26@gmail.com>
2021-01-20 10:42:48 +0200

    import App ContextProvider created

--- images-ecommerce/src/index.js
+++ images-ecommerce/src/index.js
@@ -5,6 +5,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {ContextProvider} from "./Context"
+
 ReactDOM.render(
   <React.StrictMode>
     <Router>
