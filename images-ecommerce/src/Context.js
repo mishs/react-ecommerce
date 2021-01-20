a9639e366312da62e4f9c8a366e283ec152de781
Mish <mishuct26@gmail.com>
2021-01-20 10:35:03 +0200

    Cstm cmpnent r'ring Prvider for Context created

--- images-ecommerce/src/Context.js
+++ images-ecommerce/src/Context.js
@@ -1,3 +1,13 @@
 import React from "react"
 
 const Context = React.createContext()
+
+function ContextProvider({children}){
+    // ({children}) is equal to props.children. Just that here did destructure
+    return (
+        <Context.Provider>
+            {children}
+            {/* since it's a custom component (to wrap around whole component in our app) */}
+        </Context.Provider>
+    )
+}
