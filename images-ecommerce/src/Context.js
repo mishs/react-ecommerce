0a172eaec6971629755ca96e28074b3137201270
Mish <mishuct26@gmail.com>
2021-01-20 10:59:28 +0200

    State arr var assigned to useState as 1of2 params

--- images-ecommerce/src/Context.js
+++ images-ecommerce/src/Context.js
@@ -3,6 +3,7 @@ import React, {useState} from "react"
 const Context = React.createContext()
 
 function ContextProvider({children}){
+    const [allPhotos, setAllPhotos] = useState([])
     // ({children}) is equal to props.children. Just that here did destructure
     return (
         <Context.Provider value="">
