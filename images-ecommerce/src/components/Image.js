ea5ca5cc03a8fa6387c8d22ae41df1991007814d
Mish <mishuct26@gmail.com>
2021-01-24 16:12:26 +0200

    setting default state for hover

--- images-ecommerce/src/components/Image.js
+++ images-ecommerce/src/components/Image.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react"
 
 function Image({className, imgProp}) {
+    const [isHovered, setHovered] = useState(false)
     return (
         <div className={`${className} image-container`}>
             <img src={imgProp.url} alt="each for sell" className="image-grid"/>
