2ce5b7ba856c51b133fc73d9748c753ea8aaf855
Mish <mishuct26@gmail.com>
2021-01-24 13:48:13 +0200

    Add image styling via className to img element

--- images-ecommerce/src/components/Image.js
+++ images-ecommerce/src/components/Image.js
@@ -3,7 +3,7 @@ import React from "react"
 function Image({className, photoProp}) {
     return (
         <div>
-            <img src={photoProp.url} />
+            <img src={photoProp.url} className="image-grid"/>
         </div>
     )
 }
