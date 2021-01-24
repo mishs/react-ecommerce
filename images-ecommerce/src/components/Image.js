2c8cd4552e2b59480c0bfdc82fff816449ff8258
Mish <mishuct26@gmail.com>
2021-01-24 16:15:25 +0200

    event-handle callback to handle event

--- images-ecommerce/src/components/Image.js
+++ images-ecommerce/src/components/Image.js
@@ -3,7 +3,11 @@ import React, {useState} from "react"
 function Image({className, imgProp}) {
     const [isHovered, setHovered] = useState(false)
     return (
-        <div className={`${className} image-container`}>
+        <div 
+        className={`${className} image-container`}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        >
             <img src={imgProp.url} alt="each for sell" className="image-grid"/>
         </div>
     )
