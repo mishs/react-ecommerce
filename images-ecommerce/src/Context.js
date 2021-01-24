a5c9946a24f7157b9aca01537ce92bda9d0ac99f
Mish <mishuct26@gmail.com>
2021-01-24 15:31:22 +0200

    clear log

--- images-ecommerce/src/Context.js
+++ images-ecommerce/src/Context.js
@@ -14,7 +14,6 @@ function ContextProvider({children}){
         .then(data => setAllPhotos(data))
   }, [])
 
-  console.log(allPhotos)
     return (
         <Context.Provider value={{allPhotos}}>
             {children}
