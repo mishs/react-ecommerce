075eaa99c56377460813e7fb83ca2c4fe27cb6a5
Mish <mishuct26@gmail.com>
2021-01-24 13:40:05 +0200

    Enable & make av'ble allphotos data via useCntxt

--- images-ecommerce/src/pages/Photos.js
+++ images-ecommerce/src/pages/Photos.js
@@ -3,6 +3,9 @@ import Image from "../components/Image"
 import Context from "../Context"
 
 function Photos() {
+    const {allPhotos} = useContext(Context)
+
+
     return (
         <main className="photos">
             <h1>Images go here</h1>
